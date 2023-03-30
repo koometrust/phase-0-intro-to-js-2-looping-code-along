ba0d8906d9cfe4e232f3e7f8fd330c31660f6b07
Koome <judekoome360@gmail.com>
2023-03-30 15:41:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+const name = ["jude","masssi","jess"]
+function writeCards(names) {
+    const thankYouMessages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    
+    return thankYouMessages;
+  }
+
+console.log(writeCards(name));
+ 
+//for(let countDown= 10; countDown> -1; countDown --){
+  //console.log(countDown)
+//}
+
+//let countDown = 10;
+//while(countDown >=0){
+  //console.log(countDown--);
+//}
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
